686613f0a0dc6f87afbf9b8a9079186ee0040c66
xClaudi <baranklaudia882@gmail.com>
2024-12-19 22:02:42 +0100

    view switch

--- BankApp/MainForm.cs
+++ BankApp/MainForm.cs
@@ -12,21 +12,48 @@ namespace BankApp
 {
     public partial class MainForm : Form
     {
+        AccountForm accForm;
+        StatForm statForm;
+        CardForm cardForm;
+
         public MainForm()
         {
             InitializeComponent();
-            AccountForm accForm = new AccountForm();
-            StatForm statForm = new StatForm();
-            CardForm cardForm = new CardForm();
+             accForm = new AccountForm();
+             statForm = new StatForm();
+             cardForm = new CardForm();
 
             this.IsMdiContainer = true;
             MainPanel.Controls.Add(accForm);
-            
+            MainPanel.Controls.Add(cardForm);
+            MainPanel.Controls.Add(statForm);
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            if (this.listView1.SelectedItems.Count == 0)
+            {
+                return;
+            }
+
+            MainPanel.Controls.Remove(accForm);
+            MainPanel.Controls.Remove(cardForm);
+            MainPanel.Controls.Remove(statForm);
+
+            if (this.listView1.SelectedItems[0].Text == "Accounts") {
+                MainPanel.Controls.Add(accForm);
+            }
+
+            if (this.listView1.SelectedItems[0].Text == "Cards")
+            {
+                MainPanel.Controls.Add(cardForm);
+            }
+
+            if (this.listView1.SelectedItems[0].Text == "Stats")
+            {
+                MainPanel.Controls.Add(statForm);
+            }
+            Console.WriteLine(this.listView1.SelectedItems[0].Text);
         }
 
         private void MainPanel_Paint(object sender, PaintEventArgs e)
